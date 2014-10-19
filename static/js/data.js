c25dca09aa8a73310a506305345e281585fa0c60
Richard-Swingler <rs14g12@soton.ac.uk>
2014-10-19 15:17:30 +0100

    Project overview gantt merged
    //NO BACKEND DATABASE LINK
    
    - hard coded data
    - no today's date line

---
+++ static/js/data.js
@@ -0,0 +1,17 @@
+var ganttData = [
+	{
+		id: 1, name: "Project Test", series: [
+			{ name: "Research", start: new Date(2010,00,03), end: new Date(2010,00,03) },
+			{ name: "Handle", start: new Date(2010,00,02), end: new Date(2010,00,05), color: "#f0f0f0" },
+			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
+			{ name: "Implement", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#f0f0f0" },
+			{ name: "Projected", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#e0e0e0" },
+
+			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) },
+			{ name: "Actual", start: new Date(2010,00,15), end: new Date(2010,01,03), color: "#f0f0f0" },
+
+			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
+			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,01), color: "#f0f0f0" }
+		]
+	}
+];
