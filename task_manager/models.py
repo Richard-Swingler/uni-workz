97cf640978c826fa87bd713c991029a7551ed1c8
Stuart Kellaway <sk18g12@soton.ac.uk>
2014-10-29 15:10:24 +0000

    Fixed Task to trigger eventlog package
    Now changing and adding a Task will create a log inside the log table.

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from rest_framework import serializers
+from eventlog.models import log
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.TextField()
+    author = models.ForeignKey(User, null=True)
 
     def __unicode__(self):
     	return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            log(user=self.author, action='Added a Task',
+                extra={"title": self.name})
+        else:
+            log(user=self.author, action='Changed a Task',
+                extra={"title": self.name})
+
+        super(Task, self).save(*args, **kwargs)
 
 class TaskSerializer(serializers.ModelSerializer):
 	class Meta:
