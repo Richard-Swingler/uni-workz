1c5564db485c14fa9db754a6a1637ce14c1cec32
Stuart Kellaway <sk18g12@soton.ac.uk>
2014-10-30 03:13:51 +0000

    Created Simple Login Process

--- uni_workz/urls.py
+++ uni_workz/urls.py
@@ -6,4 +6,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^timetable/', include('timetable.urls')),
     url(r'^projectOverview/', include('project_overview.urls')),
+
+    url(r'^accounts/login/$', 'uni_workz.views.login'),
+    url(r'^accounts/logout/$', 'uni_workz.views.logout'),
+    url(r'^accounts/loggedin/$', 'uni_workz.views.loggedin'),
+    url(r'^accounts/error/$', 'uni_workz.views.invalid_login'),
+    url(r'^accounts/auth/$', 'uni_workz.views.auth_view'),
 )
