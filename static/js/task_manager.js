3dbc456229c22fc2200d2e7cf86af5fba13c73a0
Eddikos <ea2g12@soton.ac.uk>
2014-11-24 13:24:33 +0000

    In Progress, To Do and Done separation
    for the task manager

--- static/js/task_manager.js
+++ static/js/task_manager.js
@@ -12,15 +12,33 @@ taskManager.controller('task_manager_ctrl', function($scope, $http) {
 	$scope.loadItems = function(){
 		$http.get('/api/v1/tasks/').then(function(response){
 			$scope.items = response.data;
+
+			var objects = response.data;
+
+			var reviews = new Array();
+
+			//for(var i = 0; i < objects.length; i++){
+			//	reviews.push([objects[i].progressFlag, objects[i]]);
+			//}
+
+			objects.forEach( 
+				function buildArray(object){
+					if(!reviews[object.progressFlag])
+						reviews[object.progressFlag] = [];
+					reviews[object.progressFlag].push(object);
+				}
+			);
+
+			$scope.reviews = reviews;
 		});
 	};
-	$scope.updateItem = function(data){
-		aler('>>');
-		$http.put('/api/update/').then(function(){
-			alert('might have done something');
-			alert('maybe');
-			alert('possibly');
-		});
-	}
+	// $scope.updateItem = function(data){
+	// 	aler('>>');
+	// 	$http.put('/api/update/').then(function(){
+	// 		alert('might have done something');
+	// 		alert('maybe');
+	// 		alert('possibly');
+	// 	});
+	// }
 	$scope.loadItems();
 });
