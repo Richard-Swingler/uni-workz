26972c4d52091efad7051b364298bc824810342e
Eddikos <eldar.alasgarov@hotmail.com>
2015-01-03 04:25:19 +0000

    Task Sorting finally done !!! updates on dragging
    the only problem left is some problems with dragging to an empty list
    plus, creating an empty task validation needs to be provided

--- static/js/task_manager.js
+++ static/js/task_manager.js
@@ -1,5 +1,5 @@
 var $jq = jQuery.noConflict();
-var taskManager = angular.module('task_manager', ['ui.bootstrap', 'ui.sortable', 'ngCookies', 'ngRoute']).config(function($httpProvider, $interpolateProvider) {
+var taskManager = angular.module('task_manager', ['ui.bootstrap', 'ui.sortable', 'ngCookies']).config(function($httpProvider, $interpolateProvider) {
    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; 
    $interpolateProvider.startSymbol('{$');
    $interpolateProvider.endSymbol('$}');
@@ -12,54 +12,21 @@ taskManager.filter('capitalize', function() {
     }
 });
 
-taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $route, $routeParams, $location) {
-	
-		$scope.$route = $route;
-     	$scope.$location = $location;
-     	$scope.$routeParams = $routeParams;
+taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $location, $window) {
+
 
 	$scope.initialize = function(data){
 		$scope.aaa = data;		
 	};
-	// $scope.updateItem = function(){
-	// 	$http({
-	// 		method: 'POST',
-	// 		url: '/api/v1/tasks/', 
-	// 		headers: {
-	// 			'X-CSRFToken': $cookies.csrftoken
-	// 		},
-	// 		data: {
-	// 	        "name": "to do 1", 
-	// 	        "description": "asdfsafd12", 
-	// 	        "author": 1, 
-	// 	        "startDate": "2014-12-07T16:23:02Z", 
-	// 	        "endDate": "2014-12-07T16:23:04Z", 
-	// 	        "progressFlag": 0
-	// 	    }
-	// 	}).
-	// 	success(function(data, status, headers, config) {
-	// 	    console.log('yay');
-	// 	}).
-	// 	error(function(data, status, headers, config) {
-	// 	    console.log($cookies.csrftoken);
-	// 	});
-	// };
+	
 	$scope.loadItems = function(){
 		$http.get('/api/v1/tasks/').then(function(response){
 			$scope.items = response.data;
 			var objects = response.data;
-			var reviews = new Array();
-			reviews = [
-			    [{
-			        "progressFlag": 0
-			    }],
-			    [{
-			        "progressFlag": 1
-			    }],
-			    [{
-			        "progressFlag": 2
-			    }]
-			]
+			$scope.reviews = new Array();
+			reviews = [[{"progressFlag": 0}],
+			    [{"progressFlag": 1}],
+			    [{"progressFlag": 2}]]
 
 			objects.forEach( 
 				function buildArray(object){
@@ -73,9 +40,6 @@ taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $r
 		});
 	}; 
 
-	
-
-
 	$scope.sortableOptions = {
 		items:'li',
 		start: function(event, ui) {
@@ -83,26 +47,50 @@ taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $r
             console.log(item.attr('id'));
             console.log(ui.item.index());
             console.log(ui.item.parent().attr('id'));
-            $scope.newList = $scope.oldList = ui.item.parent().parent();
         },
         stop: function(event, ui) {          
         	item = ui.item;
-        	console.log(ui.item);
-        	console.log(ui.item.parent().attr('id'));
-        	//alert("Moved " + item.text() + " from " + $scope.oldList.attr('id') + " to " + $scope.newList.attr('id'));
+        	var itemId = parseInt(item.attr('id'), 10);
+        	var count = 0;
+        	
+        	reviews.forEach(
+        		function buildArray(object){
+        			count++;
+        			object.forEach(
+		        		function buildArray(obj){
+							if(obj.id){
+								if(obj.id == itemId){
+								 	console.log(count);
+								 	$http({
+										method: 'PUT',
+										url: '/api/v1/tasks/' + itemId, 
+										headers: {
+											'X-CSRFToken': $cookies.csrftoken
+										},
+										data: {
+									        "name": obj.name, 
+									        "description": obj.description, 
+									        "startDate": obj.startDate, 
+									        "endDate": obj.endDate, 
+									        "progressFlag": count-1
+									    }
+									}).
+									success(function(data, status, headers, config) {
+									    console.log('Task was successfully updated in Database');
+									}).
+									error(function(data, status, headers, config) {
+									    console.log($cookies.csrftoken);
+									});
+								}	
+							}
+						}
+					);
+				}
+			);
         },
-        change: function(event, ui) {  
-            if(ui.sender) $scope.newList = ui.placeholder.parent().parent();
-            console.log(ui.sender);
-        },
-        receive: function(event, ui) {
-	        var sourceList = ui.sender;
-	        var targetList = $jq(this);
-	    },
 	    connectWith: ".apps-container",
 	};
 	
-
 	$scope.addATask = function($flag, $author){
 		var self = this;
 		console.log('User clicked submit with ',
@@ -153,7 +141,8 @@ taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $r
 		    }
 		}).
 		success(function(data, status, headers, config) {
-		    console.log('Task was successfully deleted from Database');
+		    console.log('Task was successfully updated in Database');
+		    $window.location.reload();
 		}).
 		error(function(data, status, headers, config) {
 		    console.log($cookies.csrftoken);
@@ -170,21 +159,20 @@ taskManager.controller('task_manager_ctrl', function($scope, $http, $cookies, $r
 		}).
 		success(function(data, status, headers, config) {
 		    console.log('Task was successfully deleted from Database');
+		    $window.location.reload();
 		}).
 		error(function(data, status, headers, config) {
 		    console.log($cookies.csrftoken);
 		});
 	}
 
-
 	$scope.loadItems();
-	//$scope.updateItem();
 });
-
 
 var addManagerModal = function($scope, $modal) {
 	$scope.open = function ($task) {
 		$scope.task = $task;
+		
 		// These variables are for Form Input models so the input fields are filled with relvant data when you press on a task
 		$scope.task_name = $task.name;
 		$scope.description = $task.description;
@@ -194,15 +182,13 @@ var addManagerModal = function($scope, $modal) {
 		$scope.assignedUser = $task.assignedUser;
 		$scope.taskId = $task.id;
 		// It ends hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
 		var modalInstance = $modal.open({
 	    	templateUrl: 'addManager',
 	    	scope: $scope,
 	      	controller: ModalInstanceCtrl
 	    });
 	};
-
-	
-
 
 	$scope.add = function ($flag) {
 		$scope.flag = $flag;
@@ -214,17 +200,15 @@ var addManagerModal = function($scope, $modal) {
 	};
 };
 
-var ModalInstanceCtrl = function ($scope, $modalInstance, $route) {
+var ModalInstanceCtrl = function ($scope, $modalInstance, $window) {
 	$scope.ok = function () {
 	    $modalInstance.close();
-	    $route.reload();
+	    $window.location.reload();
 	};
 	$scope.cancel = function () {
 	    $modalInstance.dismiss('cancel');
 	};
 };
-
-
 
 taskManager.controller('DatepickerDemoCtrl', function ($scope) {
 	$scope.today = function() {
@@ -235,6 +219,7 @@ taskManager.controller('DatepickerDemoCtrl', function ($scope) {
 	$scope.clear = function () {
 	    $scope.dt = null;
 	};
+
 	$scope.today1 = function() {
 	    $scope.dt1 = new Date();
 	};
@@ -244,26 +229,15 @@ taskManager.controller('DatepickerDemoCtrl', function ($scope) {
 	    $scope.dt1 = null;
 	};
 
-	  // Disable weekend selection
-	  //$scope.disabled = function(date, mode) {
-	  //  return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-	  //};
-
-	  // $scope.toggleMin = function() {
-	  //   $scope.minDate = $scope.minDate ? null : new Date();
-	  // };
-	  // $scope.toggleMin();
 	$scope.todate = function($event) {
 	    $event.preventDefault();
 	    $event.stopPropagation();
-
 	    $scope.opened = true;
 	};
 
 	$scope.todate1 = function($event) {
 	    $event.preventDefault();
 	    $event.stopPropagation();
-
 	    $scope.opened1 = true;
 	};
 
