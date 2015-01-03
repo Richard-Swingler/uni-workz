299d5bc56eb494416df46f87926609faccd3ceec
gc1e12 <gc1e12@soton.ac.uk>
2015-01-03 17:57:09 +0000

    working timetable.

--- timetable/views.py
+++ timetable/views.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 from rest_framework import viewsets
 from timetable.models import TimetableItem
 from timetable.serializers import TimetableItemSerializer
 
 def index(request):
-    return render(request, 'timetable/meetingScheduler.html')
+	if request.user.is_authenticated():
+		user_id = request.user.id
+		user_name= request.user.first_name
+	else:
+		user_id = None
+		user_name = None
+
+	return render(request, 'timetable/meetingScheduler.html', {'user_id': user_id, 'user_name':user_name})
 
 class TimetableItemViewSet(viewsets.ModelViewSet):
     """
@@ -12,3 +20,4 @@ class TimetableItemViewSet(viewsets.ModelViewSet):
     """
     queryset = TimetableItem.objects.all()
     serializer_class = TimetableItemSerializer
+
